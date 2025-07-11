4d184af70a515fe491fffded332cf840bd18f48f
VaibhavDewdhe <vdewdhe@gmail.com>
2025-07-11 15:46:47 +0530

    all code files added till date

---
+++ CodeFiles/batch_A/day10/StaticType.java
@@ -0,0 +1,7 @@
+class StaticType{
+    public static void main(String [] args){
+        int a = 10;
+        a = -56;
+        System.out.println(a);
+    }
+}
---
+++ CodeFiles/batch_A/day10/kahipan.js
@@ -0,0 +1,8 @@
+let a = prompt("Enter the first number: ")
+let b = prompt("Enter the second number: ")
+
+a = parseInt(a);
+b = parseInt(b);
+
+let c = a % b;
+alert("answer = " + c); 
