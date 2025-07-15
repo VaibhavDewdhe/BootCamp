0a4f51d7992b7494ada2b0ed495eb92e2c1acd60
VaibhavDewdhe <vdewdhe@gmail.com>
2025-07-15 12:53:44 +0530

    day13 batch code files added

---
+++ CodeFiles/batch_A/js days/day13/pattern.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 5; i++) {
+    let s = "";
+    for (let j = 1; j <= 5; j++) {
+        if(j >= i)
+            s = s + "* ";
+        else
+            s = s + "  ";
+    }
+    console.log(s);
+}
